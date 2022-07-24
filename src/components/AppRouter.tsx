146c7173012094fc953c13e250970f807ebd76a4
Дмитрий Пётух <mr.petuhdima@mail.ru>
2022-07-24 17:09:51 +0300

    install antd

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
-import Login from "../pages/Login";
 import { privateRoutes, publicRoutes } from "../routes";
 
 const AppRouter: FC = () => {
