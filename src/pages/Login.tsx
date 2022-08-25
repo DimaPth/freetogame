bd43f68f13de0a41c5ddac36dd749d2211b0e38b
Дмитрий Пётух <mr.petuhdima@mail.ru>
2022-08-25 12:31:08 +0300

    add Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from "react";
+import SignUpForm from "../components/SignUpForm/SignUpForm";
 
 const Login: FC = () => {
-  return <div>Login</div>;
+  return (
+    <div className="container">
+      <SignUpForm type="login" />
+    </div>
+  );
 };
 
 export default Login;
