52e7adc23f2c0897c89cceee267c9aa173a735b2
Дмитрий Пётух <mr.petuhdima@mail.ru>
2022-08-25 12:31:21 +0300

    add Register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from "react";
+import SignUpForm from "../components/SignUpForm/SignUpForm";
 
 const Register: FC = () => {
-  return <div>Register</div>;
+  return (
+    <div className="container">
+      <SignUpForm type="register" />
+    </div>
+  );
 };
 
 export default Register;
