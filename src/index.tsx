756be1450354be69abe74ed9b3e3b0d1d25c95f0
Дмитрий Пётух <mr.petuhdima@mail.ru>
2022-09-16 16:08:07 +0300

    redux-persist config

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from 'redux-persist/integration/react';
 import App from "./App";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 import "./firebase";
 
 const root = ReactDOM.createRoot(
@@ -12,7 +13,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,44 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { 
+  persistStore, 
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER, 
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { freeToGameApi } from './freeToGameApi'
+import { localStorageSlice } from './slices/localStorageSlice'
 import { userSlice } from './slices/userSlice'
 
-export const store = configureStore({
-  reducer: {
-    [freeToGameApi.reducerPath]: freeToGameApi.reducer,
-    user: userSlice.reducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(freeToGameApi.middleware),
+const rootReducer = combineReducers({
+  [freeToGameApi.reducerPath]: freeToGameApi.reducer,
+  user: userSlice.reducer,
+  local: localStorageSlice.reducer,
 })
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['local'],
+}
+
+ 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }).concat(freeToGameApi.middleware),
+})
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
